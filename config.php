972629f65a326aba7b6a2ba9503db0da555a105d
Linhbeng <vuthilinhbeng@gmail.com>
2022-08-29 22:12:19 +0700

    new

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3333');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'blackrouge');
+   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
